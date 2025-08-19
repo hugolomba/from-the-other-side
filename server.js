95d1af955261f4d6115d455710fbcc2be1136411
Hugo Lomba <98366477+hugolomba@users.noreply.github.com>
2025-08-19 12:40:28 +0100

    serve front end

--- server.js
+++ server.js
@@ -3,22 +3,10 @@ import { serveStatic } from './utils/serveStatic.js'
 
 const PORT = 8000
 
-/*
-Get the name of the directory holding this server.js file and store it to a const ‘__dirname’.
-*/
-
 const __dirname = import.meta.dirname
 
-
-const server = http.createServer((req, res) => {
-
-/*
-Import and call serveStatic and pass it the directory of this current module.
-*/
-    serveStatic(__dirname)
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/html')
-    res.end('<html><h1>The server is working</h1></html>')
-})
+const server = http.createServer(async (req, res) => {
+    await serveStatic(req, res, __dirname)
+}) 
 
 server.listen(PORT, ()=> console.log(`Connected on port: ${PORT}`))
--- utils/serveStatic.js
+++ utils/serveStatic.js
@@ -1,20 +1,41 @@
-/*
-Challenge 2:
-
-1. Create and export a function called 'serveStatic'. 
-   It should take in the base directory as a parameter.
-
-2. Build a path to index.html in the 'public' folder and save it to a const 'filePath'. 
-   (Which node module will you need to import to do this? Which method joins the path together?)
-
-3. Log 'filePath' to the console.
-*/
-
 import path from 'node:path'
+import fs from 'node:fs/promises'
+import { sendResponse } from './sendResponse.js'
+import { getContentType } from './getContentType.js'
 
-export function serveStatic(baseDir) {
+export async function serveStatic(req, res, baseDir) {
 
-  const filePath = path.join(baseDir, 'public', 'index.html')
-  console.log(filePath)
+  const publicDir = path.join(baseDir, 'public')
+  const filePath = path.join(
+    publicDir,
+    req.url === '/' ? 'index.html' : req.url
+  )
+
+  const ext = path.extname(filePath)
+
+  const contentType = getContentType(ext)
+
+  try {
+    const content = await fs.readFile(filePath)
+    sendResponse(res, 200, contentType, content)
+
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      const content = await fs.readFile(path.join(publicDir, '404.html'))
+      sendResponse(res, 404, 'text/html', content)
+    } else {
+      sendResponse(res, 500, 'text/html', '<html><h1>Server Error: ${err.code}</h1></html>')
+    }
+/*
+Challenge:
+
+ If the error code is “ENOENT”, serve the 404.html page.  
+ If there’s another error, serve a 500 with this string: 
+ `<html><h1>Server Error: ${err.code}</h1></html>`. 
+
+The Content-Type for the 500 can be ‘text/html’.
+*/
+    console.log(err.code)
+  }
 
 }
